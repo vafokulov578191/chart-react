32e96c982f48c81a4a0f08804a418d03a69c951a
Aminjon <vafokulov552@gmail.com>
2022-09-07 02:11:22 +0500

    regist: update

--- src/Components/Pages/Regist/Regist.jsx
+++ src/Components/Pages/Regist/Regist.jsx
@@ -1,27 +1,63 @@
 import { Box, Button, Input, Text } from '@chakra-ui/react';
 import React, { useState } from 'react'
-import { useForm } from 'react-hook-form'
+import { useEffect } from 'react';
 import { Link } from 'react-router-dom'
+import axios from '../../api/axios'
 
+let LOGIN_URL = '/register'
 
 const Regist = () => {
-    const { register, handleSubmit } = useForm();
-    const [data, setData] = useState('')
+    let [user, setUser] = useState('')
+    let [pwd, setPwd] = useState('')
+    let [Post, setPost] = useState([])
 
-    console.log(data);
+    useEffect(() => {
+        const fetchPosts = async () => {
+            try {
+                const response = await axios.get('/users');
+                setPost(response.data);
+            } catch (err) {
+                if (err.response) {
+                    // Not in the 200 response range 
+                    console.log(err.response.data);
+                    console.log(err.response.status);
+                    console.log(err.response.headers);
+                } else {
+                    console.log(`Error: ${err.message}`);
+                }
+            }
+        }
+
+        fetchPosts();
+    }, [])
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        const newPost = { username: user, password: pwd }
+        console.log(newPost);
+        try {
+            const res = await axios.post(LOGIN_URL, { newPost })
+            const allPosts = [...Post, res.data];
+            setPost(allPosts)
+            setUser('')
+            setPwd('')
+        } catch (err) {
+            console.log(`Error: ${err.message}`);
+        }
+    }
+
 
     return (
         <Box bg='#110929' w='100%' h='100vh'>
-            <form onSubmit={handleSubmit((data) => setData(JSON.stringify(data)))}
+            <form onSubmit={(e) => handleSubmit(e)}
                 style={{
-                    width: '600px', height: '100%', display: 'flex', flexDirection: 'column', margin: '0px auto', gap: '30px', marginTop: '50px',
+                    width: '600px', height: '100%', display: 'flex', flexDirection: 'column', margin: '0px auto', gap: '30px', marginTop: '150px',
                     textAlign: 'center', color: 'white'
                 }}>
                 <Text>FORM</Text>
-                <Input {...register('Name')} placeholder='Name' />
-                <Input {...register('Surname')} placeholder='Surname' />
-                <Input {...register('Age')} placeholder='Age' type={'number'} />
-                <Input {...register('Email')} placeholder='Email' />
+                <Input value={user} onChange={(e) => setUser(e.target.value)} placeholder='Name' />
+                <Input value={pwd} onChange={(e) => setPwd(e.target.value)} placeholder='password' />
                 <Input bg='green' color='white' cursor='pointer' type='submit' />
                 <Link to={'/login'}>
                     <Button bg='blue' w='100%' color='white'>Войти</Button>
